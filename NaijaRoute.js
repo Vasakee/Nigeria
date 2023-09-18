603e0928c08a2b1dca3f1b59eac4eeb2800e5f3b
BASIL AST <basilast@BASILs-MBP.mshome.net>
2023-09-18 13:25:42 +0100

    first commit

--- NaijaRoute.js
+++ NaijaRoute.js
@@ -3,7 +3,7 @@ const { CreateState, GetStates, GetState } = require('./NaijaController')
 
 NaijaRoutes.post('/', (CreateState))
 NaijaRoutes.get('/', (GetStates))
-//NaijaRoutes.get('/:stateName', (GetState))
+NaijaRoutes.get('/:stateName', (GetState))
 
 
 module.exports = NaijaRoutes
